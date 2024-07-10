b126e0392b3c3950f512949ff1f0b87399e52924
Hüseyin Karataş <huseyinlora.karatas@gmail.com>
2024-07-10 12:35:49 +0300

    [Refactor]: Navbar to Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <section id='header' className='max-w-screen-xl mx-auto h-28 border rounded-xl'>
+        Header
+    </section>
+  )
+}
+
+export default Header
