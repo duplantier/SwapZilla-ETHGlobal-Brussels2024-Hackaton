eb9d45120f6392a8123dfa21686ecc2565118c1b
Hüseyin Karataş <huseyinlora.karatas@gmail.com>
2024-07-10 10:39:40 +0300

    [Build]: Skeleton of Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
-
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1 className="text-4xl font-bold">Hello World</h1>
+      <h1 className="text-4xl font-bold">ETHGlobal Brussels2024 Hackaton</h1>
     </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <section id='navbar' className='max-w-screen-xl mx-auto h-28 border rounded-xl'>
+        Navbar
+    </section>
+  )
+}
+
+export default Navbar
